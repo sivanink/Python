b3b742bac8a7c90e5b13c710314c1951b89b69b4
Sivani NK <sivanink3@gmail.com>
2024-11-11 15:53:57 +0530

    Delete studentlist.py

--- studentlist.py
+++
@@ -1,9 +0,0 @@
-student={}
-ls=[]
-n=int(input("Enter The Number Of Students:"))
-for i in range(0,n):
-	name=input("Enter The Name:")
-	age=input("Enter The Age:")
-	grade=input("Enter The Grade:")
-	student[name]=age,grade
-print(student)
